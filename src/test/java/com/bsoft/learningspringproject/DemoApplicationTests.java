b887f7006d7afebd4e44721f04fad9f1811307f6
Buhari Nasir Maiwada <maewada@live.com>
2022-07-02 05:04:50 -0400

    modified the project

--- src/test/java/com/bsoft/learningspringproject/DemoApplicationTests.java
+++ src/test/java/com/bsoft/learningspringproject/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearningSpringProjectApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
