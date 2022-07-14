a1d207cfe935b37baa78cdd30036f277fee5df66
Buhari Nasir Maiwada <maewada@live.com>
2022-07-14 16:57:59 -0400

    code structure

--- src/main/java/com/bsoft/learningspringproject/DemoApplication.java
+++ src/main/java/com/bsoft/learningspringproject/DemoApplication.java
@@ -2,12 +2,19 @@ package com.bsoft.learningspringproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 
